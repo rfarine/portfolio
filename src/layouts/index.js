b9dd198a0ebc5bc7cca80cf779b56cc5f45725ba
Rae Farine <rfarine1027@gmail.com>
2018-05-10 16:22:52 -0400

    stripping the repo of all i dont need

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,40 +1,16 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
 
-class Template extends React.Component {
-  render() {
-    const { location, children } = this.props
-    let header
-    if (location.pathname === '/') {
-      header = (
-        <h1>
-          <Link to={'/'} >
-            Gatsby Starter Blog
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3>
-          <Link to={'/'}>
-            Gatsby Starter Blog
-          </Link>
-        </h3>
-      )
-    }
-    return (
-      <div>
-        {header}
-        {children()}
-      </div>
-    )
-  }
-}
+const Template = ({ children }) => (
+  <div>
+    <Helmet title="Rae Farine | Front-end Engineer" />
+    {children()}
+  </div>
+)
 
 Template.propTypes = {
   children: React.PropTypes.func,
   location: React.PropTypes.object,
-  route: React.PropTypes.object,
 }
 
 export default Template
